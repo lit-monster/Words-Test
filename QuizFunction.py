1996515133e39d0213114fba87489132150ac3e5
urassh <84073765+urassh@users.noreply.github.com>
2022-11-07 08:31:17 +0900

    first commit

---
+++ QuizFunction.py
@@ -0,0 +1,24 @@
+import random
+
+class Quiz(object):
+    def __init__(self, dictionary):
+        self.dictionary = dictionary
+    def quiz_handler(self):
+        template = "*"*15 + '\n英単語 : {}\n日本語を入力してください\n'+ '*'*15
+
+        print("英単語クイズを開始します。")
+        switch = input("クイズを開始しますか？ y/[N] : ")
+
+        while switch == "y":
+            word = random.choice(list(self.dictionary.keys()))
+            print(template.format(word))
+            answer = input("入力してください : ")
+            if answer == "Quit":
+                return "プログラムを終了します。"
+            elif answer == self.dictionary[word]:
+                print("正解です。")
+            elif answer!= self.dictionary[word]:
+                print("不正解です。")
+                print(f'正解は[{self.dictionary[word]}]です。')
+        else:
+            return "プログラムを終了します。"
---
+++ main.py
@@ -0,0 +1,51 @@
+from QuizFunction import Quiz
+import random
+import requests
+import re
+from bs4 import BeautifulSoup
+
+url = 'https://toiguru.jp/toeic-vocabulary-list#smoothplay1'
+
+english = []
+japanese = []
+
+try:
+    response = requests.get(url)
+    soup = BeautifulSoup(response.text, 'lxml')
+    words = soup.findAll('td')
+except requests.exceptions.ConnectionError as e:
+    print("※URLが正しくありません。")
+    print(e)
+else:
+    print("requests Succefull")
+
+    for word in words:
+        word = str(word).replace('<td>', '').replace('</td>', '').replace('<br/>', '')
+        english.append(re.findall('[a-z]+', word))
+        japanese.append(re.sub("[a-zA-Z]", "", word))
+    
+    if len(english) == len(japanese):
+        print("単語を取得しました。")
+    else:
+        print("単語の取得に失敗しました。")
+
+    
+#englishリストが、二重リストになっていて、[be], [interested], [in]という状態を解消する。
+
+# questions = dict(zip(english, japanese))
+# for s, t in enumerate(questions):
+#     print(f'{s} : {t}')
+
+# quiz = Quiz(questions)
+# result = quiz.quiz_handler()
+# print(result)
+
+
+
+
+
+
+
+
+
+    
